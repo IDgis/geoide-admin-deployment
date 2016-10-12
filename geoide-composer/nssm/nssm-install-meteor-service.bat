2770bbd0816f6a57bc43fff4a42f9e03c01558a0
RobStek <rob.stekelenburg@idgis.nl>
2016-10-12 16:35:50 +0200

    service niet auto starten

--- geoide-composer/nssm/nssm-install-meteor-service.bat
+++ geoide-composer/nssm/nssm-install-meteor-service.bat
@@ -17,7 +17,7 @@ goto ok
 echo Gebruik:
 echo  nssm-install-meteor-service.bat [lokatie meteor installaties] [meteor programma naam] [meteor poort]
 echo  Hier is de programma naam (2e argument) een subfolder van de (hoofd)lokatie (1e argument).
-echo  De service naam en database naam worden hetzelfde als de programma naam
+echo  De service naam en database naam worden hetzelfde als de programma naam 
 echo  Voorbeelden:
 echo  1. bij installatie op C:\geoide-composer-test\
 echo        nssm-install-meteor-service.bat  C:  geoide-composer-test  3010
@@ -32,7 +32,7 @@ EXIT /B 1
 rem -----------------
 set METEOR_MAIN_FOLDER_=%1
 rem install folder = program name = service name = database name:
-set METEOR_PROGRAM_NAME_=%2 
+set METEOR_PROGRAM_NAME_=%2
 set MONGO_DB_NAME_=%2
 rem prefix needed for service name? e.g. meteor_%SERVICE_NAME_%
 set SERVICE_NAME_=%2
@@ -40,13 +40,15 @@ set METEOR_PORT_=%3
 rem programma lokatie nssm relatief t.o.v. deze bat file
 set NSSM_=nssm-2.24\win64\nssm.exe
 rem install service and set parameters
-%NSSM% install %SERVICE_NAME_% %METEOR_MAIN_FOLDER_%\%METEOR_PROGRAM_NAME_%\nssm\nssm-start-meteor-service.bat %METEOR_PORT_% %MONGO_DB_NAME_%  
-%NSSM% set %SERVICE_NAME_% AppDirectory %METEOR_MAIN_FOLDER_%\%METEOR_PROGRAM_NAME_%\meteor
-%NSSM% set %SERVICE_NAME_% DisplayName %METEOR_PROGRAM_NAME_%
-%NSSM% set %SERVICE_NAME_% Description "Meteor service %METEOR_PROGRAM_NAME_%"
-%NSSM% set %SERVICE_NAME_% Start SERVICE_AUTO_START
-rem %NSSM% set %SERVICE_NAME_% ObjectName LocalSystem
-%NSSM% set %SERVICE_NAME_% AppStdout %METEOR_MAIN_FOLDER_%\%METEOR_PROGRAM_NAME_%\logs\out.log
-%NSSM% set %SERVICE_NAME_% AppStderr %METEOR_MAIN_FOLDER_%\%METEOR_PROGRAM_NAME_%\logs\err.log
-rem Now start the service
-%NSSM% start %SERVICE_NAME_% 
+echo NSSM=%NSSM_% SRV=%SERVICE_NAME_% MAIN=%METEOR_MAIN_FOLDER_% PROG=%METEOR_PROGRAM_NAME_%\nssm\nssm-start-meteor-service.bat PORT=%METEOR_PORT_% DB=%MONGO_DB_NAME_%  
+%NSSM_% install %SERVICE_NAME_% %METEOR_MAIN_FOLDER_%\%METEOR_PROGRAM_NAME_%\nssm\nssm-start-meteor-service.bat %METEOR_PORT_% %MONGO_DB_NAME_%  
+rem %NSSM_% set <servicename> ObjectName <username> <password> WERKT NIET!!!!
+%NSSM_% set %SERVICE_NAME_% AppDirectory %METEOR_MAIN_FOLDER_%\%METEOR_PROGRAM_NAME_%\meteor
+%NSSM_% set %SERVICE_NAME_% DisplayName %METEOR_PROGRAM_NAME_%
+%NSSM_% set %SERVICE_NAME_% Description "Meteor service %METEOR_PROGRAM_NAME_%"
+%NSSM_% set %SERVICE_NAME_% Start SERVICE_AUTO_START
+%NSSM_% set %SERVICE_NAME_% AppStdout %METEOR_MAIN_FOLDER_%\%METEOR_PROGRAM_NAME_%\logs\out.log
+%NSSM_% set %SERVICE_NAME_% AppStderr %METEOR_MAIN_FOLDER_%\%METEOR_PROGRAM_NAME_%\logs\err.log
+rem Do not start the service because it is installed under LocalSystem
+rem This will have to be remedied by hand to use current user and password
+rem %NSSM_% start %SERVICE_NAME_% 
