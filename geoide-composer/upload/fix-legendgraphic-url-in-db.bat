66e157e524aa7a8d44625cf20b09e741797ef468
RobStek <rob.stekelenburg@idgis.nl>
2016-10-14 16:17:22 +0200

    started script to update url's in mongo db

---
+++ geoide-composer/upload/fix-legendgraphic-url-in-db.bat
@@ -0,0 +1,30 @@
+@echo off
+rem -------- INTRO -------
+rem This script fixes the legendgraphic url's of a Geoide Composer mongo database. 
+rem Start it in the \bin folder of the mongo application if the mongo command is not on the PATH.
+rem    fix-legendgraphic-url-in-db.bat [Mongo database name] [old Url] [new Url] 
+set DB_NAME_=%1
+set OLD_URL_=%2
+set NEW_URL_=%3
+rem -----------------
+rem  check arguments
+rem -----------------
+rem if DB_NAME_=="" exit 
+if "%~1"=="" goto usage
+rem if OLD_URL_=="" exit 
+if "%~2"=="" goto usage
+rem if NEW_URL_=="" exit 
+if "%~3"=="" goto usage
+goto ok
+:usage
+echo Repareert legendgraphic url's in de database van deze applicatie.
+echo Gebruik:
+echo   fix-legendgraphic-url-in-db.bat  [database]  [oude url]  [nieuwe url]  
+echo bijvoorbeeld 
+echo   fix-legendgraphic-url-in-db.bat  geoide-composer-test  localhost:3000  localhost:3010 
+EXIT /B 1
+:ok
+rem -----------------
+
+echo mongo %DB_NAME_% --eval "db.layers.find({}).forEach(function(e, i) {console.log("e", e;e.service_layers.forEach(function(e, i) {console.log("old LG", e.legendGraphic);e.legendGraphic = e.legendGraphic.replace("//%OLD_URL_%", "//%NEW_URL_%");console.log("new LG", e.legendGraphic);db.layers.save(e);});});"
+
